cdd491df6bcdcb521258446f3aa15fc821f3dff0
Ruhanbaraban <Ruhanbaraban@gmail.com>
2025-08-11 19:56:53 +0300

    pisya

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-console.log("Hello, World! It's a script.js!");
+const city = "Владикавказ";
+const degrees = "22";
+console.log("'My city is ' + city");
+console.log("'Degrees in my city is ' + degrees")
