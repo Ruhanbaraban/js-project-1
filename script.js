5d22f9976d5fe70d0c6eaa841e2ffe249915879e
Ruhanbaraban <Ruhanbaraban@gmail.com>
2025-08-24 15:51:42 +0300

    js project

--- script.js
+++ script.js
@@ -1,8 +1,33 @@
-const numbers = [1, 2, 2, 2, 3, 3, 2, 3]
-let filterNumbers = []
-for(let i = 0; i < numbers.length; i++){
-    if(numbers[i - 1] !== numbers[i]){
-        filterNumbers.push(numbers[i])
-    }
-}
-console.log(filterNumbers)
+//1
+const header = document.getElementById("header");
+header.innerHTML = "Я все еще Руслан!";
+
+//2
+const header2 = document.getElementsByClassName("forEdit")[0];
+header2.style.color = "red";
+header2.style.backgroundColor = "green";
+
+//3
+const body = document.querySelector("body")
+const newElem = document.createElement("p");
+newElem.classList.add("createdPar");
+newElem.innerHTML = "Я последний"
+body.appendChild(newElem);
+
+//4
+const forDelete = document.getElementById("forDelete")
+body.removeChild(forDelete);
+
+//5
+const link = document.getElementById("link");
+link.setAttribute("href", "https://ru.wikipedia.org/wiki/Хабр");
+console.log(link.getAttribute("href"))
+
+//6
+const newElem2 = document.createElement("div");
+newElem2.classList.add("newElem2")
+body.appendChild(newElem2);
+
+//7
+const toggleElem = document.getElementById("forToggle")
+toggleElem.classList.toggle("active")
